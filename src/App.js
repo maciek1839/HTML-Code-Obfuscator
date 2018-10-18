d2e275d4566a1b969a51e08e7aeeeb7f947c474b
maciek1839 <maciek1839@gmail.com>
2018-10-19 01:02:51 +0200

    Add interaction to form

--- src/App.js
+++ src/App.js
@@ -1,33 +1,95 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Tabs, Tab, Row, Input, Collapsible, CollapsibleItem } from 'react-materialize';
-
-
-var algorithms=[
-  {name:'Html to Javascript',
-value: '1'},
-{name:'Html to Unicode characters',
-value: '2'},
-{name:'Html to overlapping layers',
-value: '3'},
-{name:'Combine 1 and 2',
-value: '4'}
-];
+import { Button, Icon, Tabs, Tab, Row, Input, Collapsible, CollapsibleItem } from 'react-materialize';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      algorithms: [
+        {
+          name: 'Html to Javascript',
+          value: '1',
+          details: {
+            steps: ['1. Do this do that. ',
+              '1. Do this do that. '],
+            usage: 'example'
+          }
+        },
+        {
+          name: 'Html to Unicode characters',
+          value: '2',
+          details: {
+            steps: [],
+            usage: ''
+          }
+        },
+        {
+          name: 'Html to overlapping layers',
+          value: '3',
+          details: {
+            steps: [],
+            usage: ''
+          }
+        },
+        {
+          name: 'Combine 1 and 2',
+          value: '4',
+          details: {
+            steps: [],
+            usage: ''
+          }
+        }
+      ], htymlTypes: [
+        { id: 'default', value: "Default" },
+        { id: 'custom', value: "Load custom" }
+      ],
+      userConfiguration: {
+        algorithmType: null,
+        algorithmDetails: null,
+        htmlType: null,
+        userCustomFile: null,
+        openInNewBrowserTab: false
+      }
+    };
 
-
-  // showAlgorithmDetails = false;
-  // showFilePicker = false;
-
-  constructor() {
-    super();
+    this.handleChangeAlgorithms = this.handleChangeAlgorithms.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeHtmlType = this.handleChangeHtmlType.bind(this);
+    this.handleOpenInNewBrowserTab = this.handleOpenInNewBrowserTab.bind(this);
   }
 
-  showAlgorithmDetails = () => {
+  handleChangeAlgorithms(event) {
+    let uConfig = { ...this.state.userConfiguration }
+    uConfig.algorithmType = event.target.value;
+    uConfig.algorithmDetails = this.state.algorithms.filter(e => e.value === event.target.value)[0].details;
+    this.setState({ userConfiguration: uConfig });
+  }
 
+  handleChangeHtmlType(event) {
+    let uConfig = { ...this.state.userConfiguration }
+    uConfig.htmlType = event.target.value;
+    this.setState({ userConfiguration: uConfig });
+  }
+
+  handleSubmit(event) {
+    //process
+  }
+
+  handleUserFile(event) {
+    let file = event.target.files[0];
+    let uConfig = { ...this.state.userConfiguration };
+    uConfig.userCustomFile = file;
+    this.setState({ userConfiguration: uConfig });
+  }
+
+  handleOpenInNewBrowserTab(event) {
+    let openInNewBrowser = event.target.value;
+    let uConfig = { ...this.state.userConfiguration };
+    uConfig.openInNewBrowserTab = openInNewBrowser;
+    this.setState({ userConfiguration: uConfig });
   }
 
   render() {
@@ -38,46 +100,48 @@ class App extends Component {
         </header>
         <Tabs>
           <Tab title="Configuration" active>
-            <h2>Algorithm</h2>
             <Row>
-              <Input s={12} type='select' label="Select algorithm"  >
-              
-              {this.algorithms.map(item => (
-                  <option key={item.value} value={item.value}>
-                    {item.name}
-                  </option>
-                ))}
-
-              </Input>
+              <h2>Algorithm</h2>
+              <Row>
+                <Input s={12} type='select' label="Select algorithm" defaultValue='' onChange={this.handleChangeAlgorithms}>
+                  <option value="" disabled>Choose an algorithm</option>
+                  {this.state.algorithms.map((item) =>
+                    <option key={item.value} value={item.value}>{item.name}</option>
+                  )}
+                </Input>
+              </Row>
+              {this.state.userConfiguration.algorithmDetails != null ? <Collapsible popout>
+                <CollapsibleItem header='Details' icon='notes'>
+                  {this.state.userConfiguration.algorithmDetails.steps.map((step, index) =>
+                    <span key={index}>{step}<br /> </span>
+                  )}
+                  <h3>How to use it on you page?</h3>
+                  {this.state.userConfiguration.algorithmDetails.usage}
+                </CollapsibleItem>
+              </Collapsible> : null}
+              <h2>HTML to obfuscate</h2>
+              <Row>
+                <Input s={12} type='select' label="HTML type" defaultValue='' onChange={this.handleChangeHtmlType}>
+                  <option value="" disabled>Choose HTML</option>
+                  {this.state.htymlTypes.map(type =>
+                    <option key={type.id} value={type.id}>{type.value}</option>
+                  )}
+                </Input>
+              </Row>
+              {this.state.userConfiguration.htmlType === 'custom' ? <Row>
+                <Input type="file" label="File" onChange={this.handleUserFile} />
+              </Row> : null}
+              <h3>Open in a new browser tab?</h3>
+              <Row>
+                <Input name='result-place' type='radio' value={true} label='Yes' checked={this.state.userConfiguration.openInNewBrowserTab==true} onClick={this.handleOpenInNewBrowserTab}/>
+                <Input name='result-place' type='radio' value={false} label='No*' checked={this.state.userConfiguration.openInNewBrowserTab==false} onClick={this.handleOpenInNewBrowserTab} />
+              </Row>
+              * The reulst will be shown in a new tab 'Result'.
+            <div className="center-align">
+                <Button type="submit" onClick={this.handleSubmit}>Process<Icon left>autorenew</Icon></Button>
+              </div>
             </Row>
-            <Collapsible popout>
-              <CollapsibleItem header='Details' icon='notes'>
-                1. Do this do that. <br />
-                2. Do this do that. <br />
-                3. Do this do that. <br />
-                4. Do this do that. <br />
-                5. Do this do that.
-    <h3>How to use it on you page?</h3>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </CollapsibleItem>
-            </Collapsible>
-            <h2>HTML to obfuscate</h2>
-            <Row>
-              <Input s={12} type='select' label="HTML type" defaultValue='1'>
-                <option value='1'>Default</option>
-                <option value='2'>Load custom</option>
-              </Input>
-            </Row>
-            <Row>
-              <Input type="file" label="File" />
-            </Row>
-            <h3>Open in a new browser tab?</h3>
-            <Row>
-              <Input name='result-place' type='radio' value='yes' label='Yes' />
-              <Input name='result-place' type='radio' value='no' label='No*' />
-            </Row>
-            * The reulst will be shown in a new tab 'Result'.
-    </Tab>
+          </Tab>
           <Tab title="HTML Preview">
             <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
