e30fe169c39e9c3f137f92dbda6f2ea9dc9101e0
maciek1839 <maciek1839@gmail.com>
2018-11-03 03:59:37 +0100

    Add real-time modyfing to result tab

--- src/components/tabs/obfuscation-output/ObfuscationOutput.js
+++ src/components/tabs/obfuscation-output/ObfuscationOutput.js
@@ -1,21 +1,28 @@
-import { Card, Tab } from "react-materialize";
+import { Card, Tab, Input, Row } from "react-materialize";
 import React, { Component } from "react";
 import htmlBeautify from 'html-beautify'
+import ContentEditable from 'react-contenteditable'
 
 class ObfuscationOutput extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-
+            html:'',
+            result:null
         };
     }
 
-    processHtml(){
+    handleChange = evt => {
+        let changedHtml=evt.target.value;
+        this.setState({result: this.processHtml(changedHtml)});
+      };
+
+    processHtml(html){
         let result=null;
         switch (this.props.config.algorithm.value) {
                         case '3':
-                            let escapedHtml = escape(this.props.config.html);
+                            let escapedHtml = escape(html);
                             console.log(escapedHtml);
                             let unescapedHtml = unescape(escapedHtml);
                             console.log(unescapedHtml);
@@ -36,12 +43,21 @@ class ObfuscationOutput extends Component {
                 {config ? <div className={"row"}>
                     <div className={"col s6"}><h4>HTML</h4></div>
                     <div className={"col s6"}><h4>Javascript</h4></div>
-                    <div className={"col s6"} 
+                    <div className={"col s6"} >
+                    <Row>
+                        <Input type='textarea' 
+                        style={{ whiteSpace: 'pre-wrap'}} 
+                        defaultValue={htmlBeautify(config.html)} 
+                        onChange={this.handleChange}/>
+                    </Row>
+                    {/* htmlBeautify(config.html) */}
+                    </div>
+                    {/* <div className={"col s6"} 
                     suppressContentEditableWarning={true} 
                     contentEditable={true}
-                    style={{ whiteSpace: 'pre-wrap' }}>{htmlBeautify(config.html)}</div>
+                    style={{ whiteSpace: 'pre-wrap' }}>{htmlBeautify(config.html)}</div> */}
                     <div className={"col s6"}
-                    style={{ wordWrap: 'break-word', whiteSpace: 'pre-wrap' }}>{this.processHtml()}</div>
+                    style={{ wordWrap: 'break-word', whiteSpace: 'pre-wrap' }}>{this.state.result}</div>
                 </div> : null}
             </div>
         );
